09613086862d5ddec27a6cdc3a7203b9853852b8
Albert K <mralbertk+github@protonmail.com>
2026-03-21 21:55:16 +0100

    Update for 2026

--- test/test_demo.py
+++
@@ -1,7 +0,0 @@
-from src.demo import say_hello
-
-def test_demo(capsys) -> None:
-    say_hello()
-    captured = capsys.readouterr()
-    assert captured.out == "hello world\n"
-    assert captured.err == ""
